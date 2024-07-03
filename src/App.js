2037bd92724f5772d612a28c89cc8e8c859a4318
seoh77 <seoh7dev@gmail.com>
2024-07-03 22:59:21 +0900

    RTK-query, axios, 라우터 라이브러리 설치

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
